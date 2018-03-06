be97e2f11432c95ae62683caed078815d5e60651
Dylan Lott <lott.dylan@gmail.com>
2018-03-06 17:48:16 -0500

    starting to add contacts route

---
+++ README.md
@@ -0,0 +1,6 @@
+# storj-node
+> implemented in Go
+
+This is an experiment to implement a storj full-node in Go. 
+
+
--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 )
 
+// define structs for types 
 type User struct {
 	Id int64 `json:"id"`
 	Username string `json:"username"`
+}
+
+type Contact struct {
+	Id int64 `json:"id"`	
 }
 
 func main() {
@@ -32,6 +37,21 @@ func main() {
 		user.Username ="admin"
 
 		ctx.JSON(user)
+	})
+
+	app.Get("/contacts", func(ctx iris.Context) {
+		users := make([]string, 0)
+		users = append(users, "user1")
+		ctx.JSON(users)
+	})
+
+	app.Get("/contacts/{id:long min(1)}", func(ctx iris.Context) {
+
+		details := make(map[string]string)
+		details["id"] = ctx.Params().Get("id")
+		details["username"] = "admin"
+
+		ctx.JSON(details)
 	})
 
 	app.Run(iris.Addr(":8080"))
